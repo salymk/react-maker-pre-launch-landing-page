41193a53dd1ce47cc16f8bb4c01f1a51738ae9d4
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-07-27 15:10:27 -0600

    style(hero section): add spacing

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ function App() {
         <section className="hero">
           <div className="container">
             <img className="hero-mobile" src={heroMobile} alt="hero mobile" />
-            <h1> Get paid for the work you love to do.</h1>
+            <h1>
+              Get paid for the work you <span>love</span> to do.
+            </h1>
             <p>
               The 9-5 grind is so last century. We believe in living life on
               your own terms. Whether you’re looking to escape the rat race or
