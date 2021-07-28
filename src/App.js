b7edd21a0ee92258d4d89ae1ec9972b820f2af19
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-07-28 17:01:19 -0600

    refactor(html): create container for h2 and p

--- src/App.js
+++ src/App.js
@@ -480,11 +480,13 @@ function App() {
           </div>
         </section>
         <section className="pricing">
-          <h2>Our pricing plans</h2>
-          <p>
-            We only make money when our creators make money. Our plans are
-            always affordable, and it’s completely free to get started.
-          </p>
+          <div className="pricing-header">
+            <h2>Our pricing plans</h2>
+            <p>
+              We only make money when our creators make money. Our plans are
+              always affordable, and it’s completely free to get started.
+            </p>
+          </div>
           <div className="pricing-container">
             <div className="pricing-card">
               {/* icon-free.svg */}
