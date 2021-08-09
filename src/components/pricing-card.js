d4274acdd039a8aad491f07ccb269c04ac59305a
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-08 20:21:14 -0600

    refactor(pricing): create pricing card component

---
+++ src/components/pricing-card.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+const PricingCard = (props) => (
+  <div className="pricing-card">
+    {/* icon-free.svg */}
+    <img className={props.imgClassname} src={props.imgSrc} alt={props.imgAlt} />
+    <div className="card-header">
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </div>
+    <div className="card-content">
+      <h2>{props.price}</h2>
+      <div className="price-features">
+        {props.features.map((feature) => (
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="9">
+              <path
+                fill="#3EE9E5"
+                d="M10.319.768l1.363 1.464-7.128 6.634L.319 4.924 1.682 3.46l2.871 2.674z"
+              />
+            </svg>
+            <p>{feature}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default PricingCard;
