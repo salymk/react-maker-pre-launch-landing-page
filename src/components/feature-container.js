e9bfdcc9bff81c25ce67678439b2a5961ace60d0
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-08 19:51:56 -0600

    refactor: rename features.scss to feature-container.scss

--- src/components/feature-container.js
+++ src/components/feature-container.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import './features.scss';
+import './feature-container.scss';
 
 const FeatureContainer = (props) => (
   <div className="feature-container">
