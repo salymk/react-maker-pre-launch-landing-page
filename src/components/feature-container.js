79f0f6ed7e94a298905ab136f34dff28b793474b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-08 19:39:17 -0600

    refactor(features): created feature component

---
+++ src/components/feature-container.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+
+const FeatureContainer = (props) => (
+  <div className="feature-container">
+    <div className="feature-svg-container">
+      <div className="svg-container">
+        <div className="svg-background" />
+        <img
+          className={props.imgClassname}
+          src={props.imgSrc}
+          alt={props.imgAlt}
+        />
+      </div>
+    </div>
+    <div className="feature-content">
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </div>
+  </div>
+);
+
+export default FeatureContainer;
--- src/components/features.js
+++ src/components/features.js
@@ -3,85 +3,41 @@ import passionsSVG from '../assets/illustration-passions.svg';
 import financialFreedomSVG from '../assets/illustration-financial-freedom.svg';
 import workAnywhereSVG from '../assets/illustration-work-anywhere.svg';
 import lifeStyleSVG from '../assets/illustration-lifestyle.svg';
+import FeatureContainer from './feature-container';
 
 const Features = () => (
   <section className="features">
-    <div className="feature-container">
-      <div className="feature-svg-container">
-        <div className="svg-container">
-          <div className="svg-background" />
-          <img
-            className="passion-svg"
-            src={passionsSVG}
-            alt="Indulge in your passion"
-          />
-        </div>
-      </div>
-      <div className="feature-content">
-        <h3> Indulge your passions</h3>
-        <p>
-          Your passions shouldn't be just for the weekend. Earn a living doing
-          what you love.
-        </p>
-      </div>
-    </div>
-    <div className="feature-container">
-      <div className="feature-svg-container">
-        <div className="svg-container">
-          <div className="svg-background" />
-          <img
-            className="financial-svg"
-            src={financialFreedomSVG}
-            alt="Gain financial freedom"
-          />
-        </div>
-      </div>
-      <div className="feature-content">
-        <h3> Gain financial freedom</h3>
-        <p>
-          Start making money work for you. There’s nothing quite like earning
-          while you sleep.
-        </p>
-      </div>
-    </div>
-    <div className="feature-container">
-      <div className="feature-svg-container">
-        <div className="svg-container">
-          <div className="svg-background" />
-          <img
-            className="lifestyle-svg"
-            src={lifeStyleSVG}
-            alt="Choose your lifestyle"
-          />
-        </div>
-      </div>
-      <div className="feature-content">
-        <h3>Choose your lifestyle</h3>
-        <p>
-          Own your daily schedule. Fancy a lie-in? Go for it! Take charge of
-          your week.
-        </p>
-      </div>
-    </div>
-    <div className="feature-container">
-      <div className="feature-svg-container">
-        <div className="svg-container">
-          <div className="svg-background" />
-          <img
-            className="work-svg"
-            src={workAnywhereSVG}
-            alt="Work from anywhere"
-          />
-        </div>
-      </div>
-      <div className="feature-content">
-        <h3> Work from anywhere</h3>
-        <p>
-          Selling online means not being pinned down. Want to work AND travel?
-          Go for it!
-        </p>
-      </div>
-    </div>
+    <FeatureContainer
+      imgClassname="passion-svg"
+      imgSrc={passionsSVG}
+      imgAlt="Indulge in your passion"
+      title="Indulge your passions"
+      description="Your passions shouldn't be just for the weekend. Earn a living doing what you love."
+    />
+
+    <FeatureContainer
+      imgClassname="financial-svg"
+      imgSrc={financialFreedomSVG}
+      imgAlt="Gain financial freedom"
+      title="Gain financial freedom"
+      description="Start making money work for you. There’s nothing quite like earning while you sleep."
+    />
+
+    <FeatureContainer
+      imgClassname="lifestyle-svg"
+      imgSrc={lifeStyleSVG}
+      imgAlt="Choose your lifestyle"
+      title="Choose your lifestyle"
+      description="Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week."
+    />
+
+    <FeatureContainer
+      imgClassname="work-svg"
+      imgSrc={workAnywhereSVG}
+      imgAlt="Work from anywhere"
+      title="Work from anywhere"
+      description="Selling online means not being pinned down. Want to work AND travel? Go for it!"
+    />
   </section>
 );
 
