caeef83a426f6ff76d2b9ad07227a44ec2cfb52b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-09 19:37:14 -0600

    refactor(): remove div parent container

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -18,38 +18,36 @@ const paidFeatures = [
 ];
 
 const Pricing = () => (
-  <div>
-    <section className="pricing">
-      <div className="pricing-header">
-        <h2>Our pricing plans</h2>
-        <p>
-          We only make money when our creators make money. Our plans are always
-          affordable, and it’s completely free to get started.
-        </p>
-      </div>
-      <div className="pricing-container">
-        <PricingCard
-          imgClassname="pricing-svg"
-          imgSrc={freeSVG}
-          imgAlt="Free pricing"
-          title="Dip your toe"
-          description="Just getting started? No problem at all! Our free plan will take you a long way."
-          price="Free"
-          features={freeFeatures}
-        />
+  <section className="pricing">
+    <div className="pricing-header">
+      <h2>Our pricing plans</h2>
+      <p>
+        We only make money when our creators make money. Our plans are always
+        affordable, and it’s completely free to get started.
+      </p>
+    </div>
+    <div className="pricing-container">
+      <PricingCard
+        imgClassname="pricing-svg"
+        imgSrc={freeSVG}
+        imgAlt="Free pricing"
+        title="Dip your toe"
+        description="Just getting started? No problem at all! Our free plan will take you a long way."
+        price="Free"
+        features={freeFeatures}
+      />
 
-        <PricingCard
-          imgClassname="pricing-svg"
-          imgSrc={paidSVG}
-          imgAlt="Paid pricing"
-          title="Dive right in"
-          description="Ready for the big time? Our paid plan will help you take your business to the next level."
-          price="Paid"
-          features={paidFeatures}
-        />
-      </div>
-    </section>
-  </div>
+      <PricingCard
+        imgClassname="pricing-svg"
+        imgSrc={paidSVG}
+        imgAlt="Paid pricing"
+        title="Dive right in"
+        description="Ready for the big time? Our paid plan will help you take your business to the next level."
+        price="Paid"
+        features={paidFeatures}
+      />
+    </div>
+  </section>
 );
 
 export default Pricing;
