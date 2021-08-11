d5a15b6f73c9e35ae95e4a38975635deb0471f42
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-10 20:15:50 -0600

    feat(form): add form validation with formik
    - install and configure formik

--- src/components/cta.js
+++ src/components/cta.js
@@ -1,25 +1,67 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { useFormik } from 'formik';
 import bgFooterSquiggle from '../assets/bg-footer-squiggle.svg';
 
-const CTA = () => (
-  <section className="cta">
-    <img
-      className="bg-footer-squiggle"
-      src={bgFooterSquiggle}
-      alt="Background footer squiggle"
-    />
-    <h2>Get notified when we launch</h2>
-    <form className="form">
-      <input
-        name="email"
-        type="email"
-        autoComplete="email"
-        required
-        placeholder="Email address"
+// A custom validation function. This must return an object
+// which keys are symmetrical to our values/initialValues
+const validate = (values) => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Oops! Please add your email';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Oops! That doesn’t look like an email address';
+  }
+
+  return errors;
+};
+
+const CTA = () => {
+  // Pass the useFormik() hook initial form values, a validate function that will be called when
+  // form values change or fields are blurred, and a submit function that will
+  // be called when the form is submitted
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <section className="cta">
+      <img
+        className="bg-footer-squiggle"
+        src={bgFooterSquiggle}
+        alt="Background footer squiggle"
       />
-      <button type="submit"> Get notified</button>
-    </form>
-  </section>
-);
+      <h2>Get notified when we launch</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit(e);
+        }}
+        className="form"
+      >
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          placeholder="Email address"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+        <button type="submit">Get notified</button>
+      </form>
+    </section>
+  );
+};
 
 export default CTA;
