138cb1d6fc05b1c6dd919fb36d1c2748ca7c09bd
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-11 16:58:04 -0600

    feat(helmet): add helmet with title, description and theme color

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import { Helmet } from 'react-helmet';
 import CTA from './components/cta';
 import Features from './components/features';
 import Header from './components/header';
@@ -8,6 +9,12 @@ import Pricing from './components/pricing';
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Maker</title>
+        <meta name="Maker pre-launch landing page" />
+        <meta name="theme-color" content="#080C20" />
+      </Helmet>
       <Header />
       <main>
         <Hero />
