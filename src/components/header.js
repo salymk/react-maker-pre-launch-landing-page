75b102cf611b8400036908a83e2e81c7c471321b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-17 16:04:21 -0600

    refactor(logo): remove h1 as container

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,7 @@ import Logo from '../assets/logo.svg';
 
 const Header = () => (
   <header>
-    <h1>
-      <img src={Logo} alt="Cyan circle with maker text" aria-hidden="true" />
-    </h1>
+    <img src={Logo} alt="Cyan circle with maker text" aria-hidden="true" />
   </header>
 );
 
